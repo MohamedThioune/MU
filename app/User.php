48f944b64449f30f8c6b6d399f28d60bd7c098c7
Ali Maiga <maigaali.379@gmail.com>
2021-03-18 13:19:12 +0100

    user_abonné resolution

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Models\channel;
+use App\Models\Channel;
 use Overtrue\LaravelLike\Traits\Liker;
 
 
