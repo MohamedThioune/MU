6998ecb91ff6d306f180db24bbfe0726981ae5ac
Mohamed Thioune <mokhouthioune96@gmail.com>
2021-03-23 13:00:20 +0000

    instanc

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -12,8 +12,9 @@
 </head>
 <body>
 
-<div class="d-block">
+
 @section('content-play-element')
+<div class="d-block">
 <div class="elementFixedMobie">
     <!--    <div class="content-home">
         <div class="content-naveBar">
