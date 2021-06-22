c57aefcce766e107837a352daa5af9f53a20f40c
mohamed_thioune <mokhouthioune96@gmail.com>
2021-06-22 14:59:39 +0000

    bug new user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,99 +73,12 @@
                 </div>
             </div>
         @endif
-        @if($video)
-            <div class="recondation-video-card">
-                <a href="{{route('play', $video->id)}}" class="video-vignette-recom">
-                    @if($video->thumbnail)
-                        <img src="{{asset('vids/thumbnails/') }}/{{$video->thumbnail}}" class="vignette-video-sugg">
-                    @else
-                        <img src="{{asset('img/grille.png')}}" class="vignette-video-sugg">
-                    @endif
-                </a>
-                @php 
-                    $date = explode(":",$video->duration);
-                    if($date && count($date) > 0)
-                        if($date[0] = "00")
-                            $video->duration = $date[1] . ":" . $date[2];
-                @endphp
-                <p class="durationVideo2">{{$video->duration}}</p>
-                <div class="vignette-vid-info-recom">
-                    <div class="div-block-295">
-                    <a href="{{route('play',$video->id)}}" class="titre-v-c-recom" style="text-decoration:none">{{$video->main_title}}</a>
-                        <div class="badge-eval">
-                            <img src="{{asset('img/Mu-badge-vide2x.png')}}" class="image-111">
-                        </div>
-                    </div>
-                    <div class="resume-vigne-stat">
-                        <div class="bull-name-stat">
-                            @php
-                                $channel = DB::Table('users')->select('channels.*')
-                                    ->join('channels', 'users.id', 'channels.user_id')
-                                    ->where('users.id', $video->user_id)
-                                    ->first();
-
-                                $playlist = DB::Table('playlists')->select('playlists.*')
-                                    ->where('playlists.user_id', Auth::id())
-                                    ->where('playlists.video_id', $video->id)
-                                    ->first();
-                            @endphp
-                            @if($channel)
-                            <a href="{{route('channel.visitor', $channel->id)}}" class="bull-id-acount-recom w-inline-block">
-                                <img src="{{ asset('/img') }}/{{$channel->logo}}" class="image-109">
-                            </a>
-                            
-                            <div class="div-block-292">
-                                <a href="{{route('channel.visitor', $channel->id)}}" style="text-decoration:none" class="smuusin-name-recom">{{$channel->name}}</a>
-                                <div>
-                                    
-                                    @if(intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400) == 0)
-                                    @if(intval(abs(strtotime("now") - strtotime($video->created_at))/ 3600) > 0)
-                                    <p class="publication-info-recom">{{intval(abs(strtotime("now") - strtotime($video->created_at))/3600)}} hours ago </p>
-                                    @else(intval(abs(strtotime("now") - strtotime($video->created_at))/ 3600) == 0)
-                                    <p class="publication-info-recom">{{intval(abs(strtotime("now") - strtotime($video->created_at))/60)}} minutes ago </p>
-                                    @endif
-                                    @elseif(intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400) == 1)
-                                    <p class="publication-info-recom">Yesterday at {{strftime("%H:%M", strtotime($video->created_at))}}</p>
-                                    @elseif(intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400) >= 2 && intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400) <= 27)
-                                    <p class="publication-info-recom"> {{intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400)}} days ago </p>
-                                    @elseif(intval(abs(strtotime("now") - strtotime($video->created_at))/ 86400) > 30)
-                                    <p class="publication-info-recom">{{intval(abs(strtotime("now") - strtotime($video->created_at))/ 2592000) }} months ago</p>
-                                    @else(intval(abs(strtotime("now") - strtotime($video->created_at))/ 2592000) > 12)
-                                    <p class="publication-info-recom">{{intval(abs(strtotime("now") - strtotime($video->created_at))/ 2592000 * 12) }} years ago</p>
-                                    @endif
-                                </div>
-
-                                <div class="div-block-294">
-                                    <p class="text-block-301">100k</p>
-                                    <div class="div-block-293">
-                                        <img src="{{asset('img/Mu-eye-lune-3333332x.png')}}" class="image-110">
-                                    </div>
-                                </div>
-                                
-                            </div>
-                            @endif
-                        </div>
-                    </div>
-
-                </div>
-                @if($playlist && $video)
-                <div class="d-flex justify-content-between blockPlayist" >
-                    <p class="numberviewsSuggestion"> </p>
-                    <a  href="{{route('playlist.remove', $video->id)}}" data-toggle="tooltip" data-placement="top" title="Remove to my playlist"><img src="{{ asset('img/report-Orange.svg') }}"  alt="Remove to my playlist"></a>
-                </div>
-                @else
-                <div class="d-flex justify-content-between blockPlayist" >
-                    <p class="numberviewsSuggestion"> </p>
-                    <a href="{{route('playlist.add', $video->id)}}" data-toggle="tooltip" data-placement="top" title="Add to my playlist"><img src="{{ asset('img/report-gris.svg') }}"  alt="Add to my playlist"></a>
-                </div>
-                @endif
-            </div>
-        @endif
-
+        
         <div class="hot-notification-copy-copy-copy">
             <div class="hot-green">
                 <img src="{{asset('img/Mu-hot-info.png')}}" class="image-131">
             </div>
+           
         </div>
     </div>
     <div id="event" class="big-event spaceElementFlow">
